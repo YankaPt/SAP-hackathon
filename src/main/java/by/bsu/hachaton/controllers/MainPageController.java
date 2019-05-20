cf6181c3e79ce673b5456b4b71ca06fe2b8f62f4
Egerag <echepcov@mail.ru>
2019-05-20 22:34:16 +0300

    integreted moduls

---
+++ src/main/java/by/bsu/hachaton/controllers/MainPageController.java
@@ -0,0 +1,23 @@
+package by.bsu.hachaton.controllers;
+
+import by.bsu.hachaton.DTO.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping(value = "/page")
+    public String showMainPage(Model model) {
+//        List<Request> requests = new ArrayList<>();//stub; remove with service's method
+//        Request request = new Request("RequestA", "somePhotoURL", 27.55, 54.0, "lorem ipsum", 1.0, "underWork");
+//        requests.add(request);
+//        model.addAttribute("requests", requests);
+        return "page";
+    }
+}
